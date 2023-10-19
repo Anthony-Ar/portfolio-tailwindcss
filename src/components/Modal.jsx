5663a339c56a3df0a4bb42e6c78f2dff52f593f9
Anthony <anthony.arrighi@orange.fr>
2023-10-19 19:35:14 +0200

    live version button

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,6 +4,7 @@ import {
     XMarkIcon,
     DevicePhoneMobileIcon,
     ComputerDesktopIcon,
+    ArrowTopRightOnSquareIcon
 } from "@heroicons/react/24/outline";
 
 export default function Modal({ onClose, data }) {
@@ -109,27 +110,41 @@ export default function Modal({ onClose, data }) {
                                     />
                                 )}
 
-                                {data.external_links.github !== "" && (
-                                    <div className="mt-6 flex justify-center">
-                                        <a
-                                            href={data.external_links.github}
-                                            className="flex items-center gap-2 rounded border border-stone-100 px-6 py-2 duration-300 hover:cursor-pointer hover:border-stone-700 dark:border-[var(--color-background)] dark:text-[var(--color-text)] dark:hover:border-[var(--color-red)]"
-                                        >
-                                            <svg
-                                                viewBox="0 0 24 24"
-                                                aria-hidden="true"
-                                                className="h-5 w-5 dark:fill-[var(--color-text)]"
+                                <div className="flex justify-center gap-4">
+                                    {data.external_links.github !== "" && (
+                                        <div className="mt-6 flex justify-center">
+                                            <a
+                                                href={data.external_links.github}
+                                                className="flex items-center gap-2 rounded border border-stone-100 px-6 py-2 duration-300 hover:cursor-pointer hover:border-stone-700 dark:border-[var(--color-background)] dark:text-[var(--color-text)] dark:hover:border-[var(--color-red)]"
                                             >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    clipRule="evenodd"
-                                                    d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
-                                                ></path>
-                                            </svg>
-                                            Code source
-                                        </a>
-                                    </div>
-                                )}
+                                                <svg
+                                                    viewBox="0 0 24 24"
+                                                    aria-hidden="true"
+                                                    className="h-5 w-5 dark:fill-[var(--color-text)]"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        clipRule="evenodd"
+                                                        d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.607 9.607 0 0 1 12 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48 3.97-1.32 6.833-5.054 6.833-9.458C22 6.463 17.522 2 12 2Z"
+                                                    ></path>
+                                                </svg>
+                                                Code source
+                                            </a>
+                                        </div>
+                                    )}
+
+                                    {data.external_links.live !== "" && (
+                                        <div className="mt-6 flex justify-center">
+                                            <a
+                                                href={data.external_links.live}
+                                                className="flex items-center gap-2 rounded border border-stone-100 px-6 py-2 duration-300 hover:cursor-pointer hover:border-stone-700 dark:border-[var(--color-background)] dark:text-[var(--color-text)] dark:hover:border-[var(--color-red)]"
+                                            >
+                                                <ArrowTopRightOnSquareIcon className="h-5 w-5 dark:text-[var(--color-text)]" />
+                                                Version live
+                                            </a>
+                                        </div>
+                                    )}
+                                </div>
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
